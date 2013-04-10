37044dcd4f610ac3c0a1827daaa99e8931b4dc67
Andrew <andrew.swerlick@gmail.com>
2013-04-10 09:43:18 -0400

    fixing active link for pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#nav-list').html(
-		"<li class='active'><a href='#'>Home</a></li>" +
+		"<li><a href='#'>Home</a></li>" +
 		"<li><a href='behavior.html'>Behavior</a></li>" +
 		"<li><a href='habitat.html'>Habitat</a></li>" +
 		"<li><a href='communication.html'>Communication</a></li>" +
@@ -11,4 +11,19 @@ $(document).ready(function() {
 		"<li><a href='diet.html'>Diet</a></li>" +
 		"<li><a href='gallery.html'>Image Gallery</a></li>"
 	);
+
+	function qualifyURL(url) {
+	    var a = $('<a></a>');
+	    a.attr("href", url);
+	    return a[0].href;
+	}
+	var currentUrl = window.location.href.split('?')[0];
+
+	$("#nav-list li").each(function (index, item) {
+	    var url = $(item).find("a").attr("href");
+	    var fullUrl = qualifyURL(url);
+	    if (currentUrl.indexOf(fullUrl) != -1)
+	        $(item).addClass("active");
+	});
+
 });
